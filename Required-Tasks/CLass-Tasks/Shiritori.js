2c3d4c32b96a2fef7e609d614692153559d9cb92
Arman773 <tadevosyan889@gmail.com>
2022-01-11 19:02:33 +0400

    feat: added new task

---
+++ Required-Tasks/CLass-Tasks/Shiritori.js
@@ -0,0 +1,36 @@
+'use strict'
+
+class Shiritori {
+    constructor(){
+        this.words = [];
+        this.gameOver = false;
+    }
+
+    play(word){
+        if (this.words.length === 0){
+            this.words.push(word);
+        } else {
+            let str = this.words[this.words.length-1];
+            str = str.slice(this.words[this.words.length-1].length-1);
+            if (str !== word[0] || this.words.includes(word)){
+                this.gameOver = true;
+                return 'Game is over!';
+            } else {
+                this.words.push(word);
+            }
+        }
+        return this.words;
+    }
+
+    restart(){
+
+    }
+}
+
+const game = new Shiritori();
+
+console.log(game.play('arman'));    // [ 'arman' ]
+console.log(game.play('namak'));    // [ 'arman', 'namak' ]
+console.log(game.play('karkut'));   // [ 'arman', 'namak', 'karkut' ]
+console.log(game.play('tari'));     // [ 'arman', 'namak', 'karkut', 'tari' ]
+console.log(game.play('qar'));      // Game is over!
